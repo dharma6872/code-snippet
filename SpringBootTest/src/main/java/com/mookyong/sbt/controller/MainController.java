f7c4069c3b38e7abfc5d0c0c38b0025ece7c48ad
Administrator <Administrator@PC082>
2017-05-21 01:06:41 +0900

    테스트 진행중

--- SpringBootTest/src/main/java/com/mookyong/sbt/controller/MainController.java
+++ SpringBootTest/src/main/java/com/mookyong/sbt/controller/MainController.java
@@ -1,13 +1,14 @@
 package com.mookyong.sbt.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 	
 	@RequestMapping("/")
 	public String index() {
-		return "i am working";
+		return "index";
 	}
 }
